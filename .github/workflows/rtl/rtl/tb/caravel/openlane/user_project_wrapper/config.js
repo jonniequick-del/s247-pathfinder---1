d37d93144cdb346defdb21b9aba732e83a147167
jonnie <jonniequick@gmail.com>
2026-02-20 14:19:30 +0000

    Create config.js

---
+++ .github/workflows/rtl/rtl/tb/caravel/openlane/user_project_wrapper/config.js
@@ -0,0 +1,27 @@
+{
+    "DESIGN_NAME": "user_project_wrapper",
+    "VERILOG_FILES": [
+        "dir::../../caravel/user_project_wrapper.v",
+        "dir::../../rtl/s247_pathfinder_top.sv",
+        "dir::../../rtl/s247_compute_core.sv"
+    ],
+    "CLOCK_PORT": "wb_clk_i",
+    "CLOCK_PERIOD": 20,
+    "DIE_AREA": "0 0 2000 2000",
+    "FP_PIN_ORDER_CFG": "dir::pin_order.cfg",
+    "FP_SIZING": "absolute",
+    "PDK": "gf180mcuD",
+    "STD_CELL_LIBRARY": "gf180mcu_fd_sc_mcu7t5v0",
+    "PL_TARGET_DENSITY": 0.45,
+    "GRT_ALLOW_CONGESTION": true,
+    "RT_MAX_LAYER": "Metal4",
+    "SYNTH_STRATEGY": "AREA 0",
+    "SYNTH_MAX_FANOUT": 8,
+    "PL_BASIC_PLACEMENT": true,
+    "CELL_PAD": 4,
+    "DIODE_INSERTION_STRATEGY": 4,
+    "RUN_CVC": false,
+    "RUN_KLAYOUT_XOR": false,
+    "FP_PDN_CHECK_NODES": false,
+    "MAGIC_ZEROIZE_ORIGIN": false
+}
